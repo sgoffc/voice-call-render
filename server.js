2c2b29d623f990752b01036294a21006fb0bd551
ՏԌ Є-Տροɾƭร <sgofic@gmail.com>
2026-02-10 13:12:39 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -9,20 +9,20 @@ const io = new Server(server, {
   cors: { origin: "*" }
 });
 
-const rooms = {}; // lista de usuários por sala
+// lista de usuários por sala
+const rooms = {};
 
 io.on("connection", socket => {
   console.log("Conectou:", socket.id);
 
   socket.on("join-room", (room, user) => {
     socket.join(room);
-    if(!rooms[room]) rooms[room] = [];
+    if (!rooms[room]) rooms[room] = [];
     rooms[room].push({ id: socket.id, ...user });
 
-    // envia lista atualizada para todos na sala
+    // envia lista completa para todos na sala
     io.to(room).emit("user-list", rooms[room]);
-
-    // avisa que novo usuário entrou
+    // avisa que alguém entrou
     socket.to(room).emit("user-joined", { id: socket.id, ...user });
   });
 
@@ -34,19 +34,20 @@ io.on("connection", socket => {
   });
 
   socket.on("leave-room", room => {
-    if(rooms[room]){
+    if (rooms[room]) {
+      const leavingUser = rooms[room].find(u => u.id === socket.id);
       rooms[room] = rooms[room].filter(u => u.id !== socket.id);
-      socket.to(room).emit("user-left", { id: socket.id });
+      socket.to(room).emit("user-left", leavingUser);
     }
     socket.leave(room);
   });
 
   socket.on("disconnect", () => {
-    for(const room in rooms){
-      const user = rooms[room].find(u=>u.id===socket.id);
-      if(user){
-        rooms[room] = rooms[room].filter(u=>u.id!==socket.id);
-        socket.to(room).emit("user-left", user);
+    for (const room in rooms) {
+      const leavingUser = rooms[room].find(u => u.id === socket.id);
+      if (leavingUser) {
+        rooms[room] = rooms[room].filter(u => u.id !== socket.id);
+        socket.to(room).emit("user-left", leavingUser);
       }
     }
     console.log("Saiu:", socket.id);
@@ -54,4 +55,4 @@ io.on("connection", socket => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, ()=>console.log("Servidor online"));
+server.listen(PORT, () => console.log("Servidor online"));
