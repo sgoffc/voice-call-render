f35f6021a59edcea2658798a7f1ecb93d1b4f823
ՏԌ Є-Տροɾƭร <sgofic@gmail.com>
2026-02-10 12:36:03 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -6,32 +6,54 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
+  cors: { origin: "*" }
 });
+
+// Lista de usuários por sala
+const rooms = {};
 
 io.on("connection", socket => {
   console.log("Conectou:", socket.id);
 
-  socket.on("join-room", room => {
+  // Entrar na sala
+  socket.on("join-room", (room, user) => {
     socket.join(room);
-    socket.to(room).emit("user-joined", socket.id);
+    socket.user = user; // guarda os dados do usuário
+    if(!rooms[room]) rooms[room] = {};
+    rooms[room][socket.id] = user;
+
+    // Envia lista completa para o novo usuário
+    socket.emit("user-list", Object.values(rooms[room]));
+
+    // Notifica os outros que entrou
+    socket.to(room).emit("user-joined", user);
   });
 
+  // Sinal WebRTC
   socket.on("signal", data => {
-    io.to(data.to).emit("signal", {
-      from: socket.id,
-      signal: data.signal
-    });
+    io.to(data.to).emit("signal", { from: socket.id, signal: data.signal });
   });
 
+  // Sair da call
+  socket.on("leave-room", (room) => {
+    if(rooms[room] && rooms[room][socket.id]){
+      delete rooms[room][socket.id];
+      socket.to(room).emit("user-left", socket.user);
+    }
+  });
+
+  // Desconexão
   socket.on("disconnect", () => {
+    for(const room in rooms){
+      if(rooms[room][socket.id]){
+        const user = rooms[room][socket.id];
+        delete rooms[room][socket.id];
+        socket.to(room).emit("user-left", user);
+      }
+    }
     console.log("Saiu:", socket.id);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () =>
-  console.log("Servidor online")
-);
+server.listen(PORT, () => console.log("Servidor online"));
