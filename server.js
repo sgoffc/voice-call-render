0699a57b3a99f2ab0d40bba96f91e6e522545772
ՏԌ Є-Տροɾƭร <sgofic@gmail.com>
2026-06-15 20:38:38 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ function pairKey(a, b) {
 const ROOM_LIMITS = {
   "sala-geral": 16,
   "sala-events": 10,
-  "sala-duo": 1,
+  "sala-duo": 2,
   "sala-duo2": 2,
-  "sala-squad": 6,
-  "sala-squad2": 6
+  "sala-squad": 4,
+  "sala-squad2": 4
 };
 
 io.on("connection", socket => {
