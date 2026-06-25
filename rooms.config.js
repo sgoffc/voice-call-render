332092f60c96b97d0c4f1aef4282863e959c3ecc
ՏԌ Є-Տροɾƭร <sgofic@gmail.com>
2026-06-24 21:19:18 -0300

    Criar rooms.config.js

---
+++ rooms.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "sala-geral": {
+    limit: 16,
+    password: null
+  },
+
+  "sala-events": {
+    limit: 10,
+    password: "123456"
+  },
+
+  "sala-duo": {
+    limit: 2,
+    password: "duo123"
+  },
+
+  "sala-duo2": {
+    limit: 2,
+    password: "duo456"
+  },
+
+  "sala-squad": {
+    limit: 4,
+    password: "squad123"
+  },
+
+  "sala-squad2": {
+    limit: 4,
+    password: "squad456"
+  }
+};
