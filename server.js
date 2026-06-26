2987f573cc604fc4966f8d72528ed11cccd93bdd
ՏԌ Є-Տροɾƭร <sgofic@gmail.com>
2026-06-26 19:47:14 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const ROOMS = {
   "sala-duo": { limit: 2, password: "duo123" },
   "sala-duo2": { limit: 2, password: "duo456" },
   "sala-squad": { limit: 4, password: "squad123" },
-  "sala-squad2": { limit: 4, password: "squad456" }
+  "sala-squad2": { limit: 4, password: "squad456" },
+  "sala-sgoffc": { limit: 10, password: "sgoffc" }
 };
 
 /* ========================= */
