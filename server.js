69093e4ffad24302d68ebe0a303778c5076c0a36
ՏԌ Є-Տροɾƭร <sgofic@gmail.com>
2026-06-29 12:56:45 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -14,11 +14,12 @@ const io = new Server(server, {
 const ROOMS = {
   "sala-geral": { limit: 16, password: null },
   "sala-events": { limit: 10, password: "123456" },
-  "sala-duo": { limit: 2, password: "duo123" },
-  "sala-duo2": { limit: 2, password: "duo456" },
-  "sala-squad": { limit: 4, password: "squad123" },
-  "sala-squad2": { limit: 4, password: "squad456" },
-  "sala-sgoffc": { limit: 10, password: "sgoffc" }
+  "sala-duo": { limit: 2, password: "null" },
+  "sala-duo2": { limit: 2, password: "null" },
+  "sala-squad": { limit: 4, password: "null" },
+  "sala-squad2": { limit: 4, password: "null" },
+  "sala-squad3": { limit: 4, password: "null" },
+  "sala-sgoffc": { limit: 10, password: "null" }
 };
 
 /* ========================= */
